1bdf02ac65fcbd68ed15dac2b7d8bb11197ae174
Ahmad Merie <ahbmer3i@gmail.com>
2021-04-08 19:51:25 +0300

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Ahmad Ahmed on 08/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
